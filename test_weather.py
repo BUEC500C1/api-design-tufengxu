e1eb3b4ba97e54dddd2f29fd5b0668c40178cefa
Fengxu Tu <55207104+tufengxu@users.noreply.github.com>
2020-02-03 22:26:51 -0500

    Add files via upload

---
+++ test_weather.py
@@ -0,0 +1,22 @@
+from weather_api import get_weather_info
+from weather_api import get_airport_info
+
+
+def main():
+    wea_dict = {}
+    user_api = 'a78abfab06d3f73865da94e3ff663935'
+    file_name = 'airports.csv'
+    airport = '00AS'
+    geo_info = get_airport_info(file_name, airport)
+    wea_dict['Airport name'] = geo_info[2]
+    wea_dict['Airport ident'] = airport
+    wea_ = get_weather_info(user_api, geo_info)
+    
+    for key in wea_:
+        wea_dict[key] = wea_[key]
+    for key in wea_dict:
+        print(key + ': ' + str(wea_dict[key]))
+
+
+if __name__ == '__main__':
+    main()
